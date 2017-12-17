edd3647df3b3b879921b807eb9cd63f664fd850c
kecarus <ken@ipl31.net>
2017-12-16 17:52:02 -0800

    attempt to make this work django 1.10

--- linkdump/urls.py
+++ linkdump/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from linkdump.views import (link_dump_list,
+                            link_dump_tag_list,
+                            link_dump_redirect)
 
 
-urlpatterns = patterns("linkdump.views",
-    url("^$", "link_dump_list", name="link_dump_list"),
-    url("^category/(?P<category>.*)/$", "link_dump_list",
+urlpatterns = [
+    url("^$", link_dump_list, name="link_dump_list"),
+    url("^category/(?P<category>.*)/$", link_dump_list,
         name="link_dump_list"),
-    url("^tags/(?P<keyword_slug>.*)/$", "link_dump_tag_list",
+    url("^tags/(?P<keyword_slug>.*)/$", link_dump_tag_list,
         name="link_dump_tag_list"),
-    url("^(?P<dump_slug>.*)/$", "link_dump_redirect",
+    url("^(?P<dump_slug>.*)/$", link_dump_redirect,
         name="link_dump_redirect"),
-)
+    ]
